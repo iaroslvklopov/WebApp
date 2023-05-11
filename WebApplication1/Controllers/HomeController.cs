83cdd94204d684f639a5a0cd09eed076bd562c88
almira <almira@email.com>
2023-05-11 20:40:26 +0300

    альмира сделала проверку статауса заявки

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -48,41 +48,73 @@ namespace WebApplication1.Controllers
             return View();
         }
 
-        
-        
 
-        [HttpPost]
-        public IActionResult TaskThird(string contactName, string contactNumber, string radio)
-        {
+
+
+		[HttpPost]
+		public IActionResult TaskThird(string contactName, string contactNumber, string radio, string type)
+		{
 			MySqlConnection conn = new MySqlConnection("server=127.0.0.1;port=3306;database=mydb;user id=root;password=1234;charset=utf8;Pooling=false;SslMode=None;");
-			try
+			if (type == "Отправить")
 			{
-				if (conn.State == ConnectionState.Closed)
-				{
-					conn.Open();
-					MySqlCommand cmd1 = new MySqlCommand("select max(manager_id) from manager", conn);
-					int log = Convert.ToInt32(cmd1.ExecuteScalar());
-					Random r = new Random();
-					int idd = r.Next(1, log+1);
-					string time = "";
-                    if (radio == "1") { time = "10:00-13:00"; }
-					if (radio == "2") { time = "13:00-16:00"; }
-					if (radio == "3") { time = "16:00-19:00"; }
-					MySqlCommand cmd = new MySqlCommand("insert into zayavka (name, number, time, manager_id) values(@name, @number, @time, @manager_id);", conn);
-					cmd.Parameters.AddWithValue("@name", contactName);
-					cmd.Parameters.AddWithValue("@number", contactNumber);
-					cmd.Parameters.AddWithValue("@time", time);
-					cmd.Parameters.AddWithValue("@manager_id", idd);
-					cmd.ExecuteNonQuery();
+				//try
+				//{
+					if (conn.State == ConnectionState.Closed)
+					{
+						conn.Open();
+						MySqlCommand cmd1 = new MySqlCommand("select max(manager_id) from manager", conn);
+						int log = Convert.ToInt32(cmd1.ExecuteScalar());
+						Random r = new Random();
+						int idd = r.Next(1, log + 1);
+						string time = "";
+						if (radio == "1") { time = "10:00-13:00"; }
+						if (radio == "2") { time = "13:00-16:00"; }
+						if (radio == "3") { time = "16:00-19:00"; }
+					    string timenow = Convert.ToString(DateTime.Now);
+					    MySqlCommand cmd = new MySqlCommand("insert into zayavka (name, number, time, timenow, manager_id, status) values(@name, @number, @time, @timenow, @manager_id, @status);", conn);
+						cmd.Parameters.AddWithValue("@name", contactName);
+						cmd.Parameters.AddWithValue("@number", contactNumber);
+						cmd.Parameters.AddWithValue("@time", time);
+					    cmd.Parameters.AddWithValue("@timenow", timenow);
+					    cmd.Parameters.AddWithValue("@manager_id", idd);
+						cmd.Parameters.AddWithValue("@status", "ожидает звонка");
+						cmd.ExecuteNonQuery();
 
-				}
-				return RedirectToAction("TaskThird");
+					}
+					return RedirectToAction("TaskThird");
+				//}
+				//catch (MySqlException ex)
+				//{
+				//	return Content("<script language='javascript' type='text/javascript'>alert(ex.ToString());</script>");
+				//}
 			}
-			catch (MySqlException ex)
+			if (type == "Узнать")
 			{
-				return Content("<script language='javascript' type='text/javascript'>alert(ex.ToString());</script>");
+				//try
+				//{
+					if (conn.State == ConnectionState.Closed)
+					{
+						conn.Open();
+						MySqlCommand cmd = new MySqlCommand("select * from zayavka where number = @number", conn);
+						cmd.Parameters.AddWithValue("@number", contactNumber);
+						MySqlDataReader reader = cmd.ExecuteReader();
+						string line = "";
+						while (reader.Read())
+						{
+							line += Convert.ToString(reader["timenow"]);
+						    line += ": ";
+							line += Convert.ToString(reader["status"]);
+						line += "\n";
+					}
+					    reader.Close();
+						ViewBag.H = line;
+					}
+				//}
+				//catch { Content("<script language='javascript' type='text/javascript'>alert(ex.ToString());</script>"); }
 			}
-
+			return View();
 		}
-    }
+
+	}
+
 }
